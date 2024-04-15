927f1f38fb38ff0c52d142ae34a36dc6622d53e7
BrainFisher <brainfisher13@gmail.com>
2024-04-15 21:58:02 +0300

    html

--- quotes_site/quotes_app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class QuoteForm(forms.Form):
-    quote_text = forms.CharField(label='Цитата', max_length=500)
-    author_name = forms.CharField(label='Автор', max_length=100)
